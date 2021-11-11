56d695574deb1001203b6c86a5ff9c12f943ef6f
Dillon Kearns <dillon@dillonkearns.com>
2021-11-11 12:51:24 -0800

    Setup for publishing.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-import fs from "fs";
+import fs from "node:fs";
 import spawn from "cross-spawn-with-kill";
-import path from "path";
+import path from "node:path";
 import openEditor from "open-editor";
 import fetch from "node-fetch";
 import { Command } from "commander/esm.mjs";
 
 let elmLiveProcess;
 
-process.on("SIGTERM", () => {
-  elmLiveProcess && elmLiveProcess.kill();
-});
-process.on("SIGINT", () => {
-  elmLiveProcess && elmLiveProcess.kill();
-  process.exit(0);
-});
-process.on("exit", () => {
-  elmLiveProcess && elmLiveProcess.kill();
-});
-
-start();
-
-function start() {
+export function start() {
+  process.on("SIGTERM", () => {
+    elmLiveProcess && elmLiveProcess.kill();
+  });
+  process.on("SIGINT", () => {
+    elmLiveProcess && elmLiveProcess.kill();
+    process.exit(0);
+  });
+  process.on("exit", () => {
+    elmLiveProcess && elmLiveProcess.kill();
+  });
   const program = new Command();
   program
     .argument("<ellie-id-or-url>", "the id or URL for an ellie-app example")
